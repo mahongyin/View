6566088b61959d0a20d7ea896449ba8fab231cd8
1976222027 <2010MA520ma>
2020-03-18 14:16:19 +0800

    添加 其他view

--- app/src/main/java/com/mhy/view/steps/MainActivity.java
+++ app/src/main/java/com/mhy/view/steps/MainActivity.java
@@ -1,6 +1,9 @@
 package com.mhy.view.steps;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
@@ -34,7 +37,7 @@ public class MainActivity extends AppCompatActivity implements StepsView.MyViewC
     private List<Integer> signinlist = new ArrayList<>();
     private List<Integer> signinStates = new ArrayList<>();
     int day = -1;//今天在一周的角标
-Activity context;
+Activity context;String changeTo;
     String[] week = {"一", "二", "三", "四", "五", "六", "日"};
     int showNum = 5;////VIP 1.2倍 SVIP 2倍  ADD 根据身份 区分
 
@@ -53,7 +56,48 @@ Activity context;
         signinlist.add(0);
 
         initView();
+
+        
+        //改变 桌面显示图
+        TextView tvInfo = (TextView) findViewById(R.id.tv_info);
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            tvInfo.setText("Name:" + getComponentName().getClassName() + "\nVersion:" + packageInfo.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
+
+    public void changeToIcon1(View v) {
+        changeTo = getClass().getName() + "1";
+    }
+
+    public void changeToIcon2(View v) {
+        changeTo = getClass().getName() + "2";
+    }
+
+    public void reset(View v) {
+        changeTo = getClass().getName();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (changeTo != null) {
+            changeLauncher(changeTo);
+        }
+        super.onDestroy();
+    }
+
+    private void changeLauncher(String name) {
+        PackageManager pm = getPackageManager();
+        //隐藏之前显示的桌面组件
+        pm.setComponentEnabledSetting(getComponentName(),
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+        //显示新的桌面组件
+        pm.setComponentEnabledSetting(new ComponentName(MainActivity.this, name),
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+    }
+
 
 
     /**
